2d9dda7f958603a88d9b43a32825c0ae0908f502
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-17 12:00:51 -0300

    sistema

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-//Modulos externos 
-const inquirer = require('inquirer')
-const chalk = require ('chalk')
+//Modulos externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 //Modulos internos
 
-const fs = require('fs')
+const fs = require("fs");
 
-console.log('Iniciando o bank')
+operations()
+
+function operations() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "O que você deseja fazer?",
+      choices: ["Criar Conta", "Consultar Saldo", "Depositar", "Sacar", "Sair"],
+    },
+  ]).then((answer) => {
+
+    const action = answer['action']
+
+    console.log(action)
+  }).catch((err) => console.log(err));
+}

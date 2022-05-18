62bed47fff80da7afd36b4589d519f9a64e413d6
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-18 10:37:22 -0300

    create an account

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ function operations() {
 
     const action = answer['action']
 
-    console.log(action)
+    if(action === 'Criar Conta') {
+      createAccount()
+    }
   }).catch((err) => console.log(err));
+}
+
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }

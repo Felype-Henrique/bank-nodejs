47e6fe09a8a53b8ad1cef1e71260a7c891ce198f
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-23 10:52:53 -0300

    widthdraw

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function operation() {
       } else if (action === "Consultar Saldo") {
         getAccountBalance();
       } else if (action === "Sacar") {
+        widthdraw()
       } else if (action === "Sair") {
         console.log(chalk.bgBlue.black("Obrigado por usar o Bank!"));
         process.exit();
@@ -200,4 +201,39 @@ function getAccountBalance() {
       operation()
     })
     .catch((err) => console.log(err));
+}
+
+//widthdraw an amount from user account
+function widthdraw(){
+
+  inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Qual o nome da sua conta ?'
+    }
+  ])
+  .then((answer) => {
+
+    const accountName = answer['accountName']
+
+    if(!checkAccount(accountName)){
+      return widthdraw()
+    }
+
+    inquirer.prompt([
+      {
+        name: 'amount',
+        message: 'Quanto você deseja sacar ?'
+      }
+    ])
+    .then((answer) => {
+
+      const amount = answer['amount']
+
+      console.log(amount)
+      operation();
+    })
+    .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
 }
